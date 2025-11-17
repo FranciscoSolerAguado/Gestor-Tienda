bd62e36ad4742c6a889b0d96f53a3420efb8fd3d
FranciscoSolerAguado <franciscojsoleraguado@gmail.com>
2025-11-17 20:40:30 +0100

    Inicio.fxml y InicioController De momento solo he creados la clae y el archivo, no tienen nada dentro

--- src/main/java/org/fran/gestortienda/controller/InicioController.java
+++ src/main/java/org/fran/gestortienda/controller/InicioController.java
@@ -1,4 +1,10 @@
 package org.fran.gestortienda.controller;
 
+import org.fran.gestortienda.utils.LoggerUtil;
+
+
+import java.util.logging.Logger;
+
 public class InicioController {
+    private static final Logger LOGGER = LoggerUtil.getLogger();
 }
